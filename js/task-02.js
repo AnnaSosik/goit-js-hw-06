eada45fe5342360947dc5ae124f0165538dbe645
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-08-05 14:52:30 +0200

    task-01.js updated

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");('ul#ingredients');
+
+for (const ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  list.append(item);
+  console.log(item);
+}
