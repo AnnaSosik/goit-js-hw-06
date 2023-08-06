9040b8391eea41a88995585ad725732a8698e5cd
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-08-06 15:24:21 +0200

    Tasks updated

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 const textInput = document.querySelector("#name-input");
-const textOutput = document.querySelector("#name-output");
+const output = document.querySelector("#name-output");
 const anonymousValue = 'Anonymous';
 
 textInput.addEventListener("input", (event) => {
     if (event.currentTarget.value) {
-        textOutput.textContent = event.currentTarget.value;
+        output.textContent = event.currentTarget.value;
     } else {
-        textOutput.textContent = anonymousValue;
+        output.textContent = anonymousValue;
     }
 });
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+      .toString(16)
+      .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+};
+
+window.onload = (event) => changeColor();
+btn.addEventListener("click", changeColor);
